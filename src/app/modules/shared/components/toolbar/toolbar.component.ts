e011467320f0275dc2de42bb6374342c773c1dff
Justyna <justynasadlak10@gmail.com>
2019-07-17 15:21:47 +0200

    Create toolbar

---
+++ src/app/modules/shared/components/toolbar/toolbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material';
+import {LoginDialogComponent} from '../../../user/login-dialog/login-dialog.component';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent implements OnInit {
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit() {
+  }
+
+  onLogin() {
+    this.dialog.open(LoginDialogComponent, {
+      width: '250px'
+    });
+  }
+}
