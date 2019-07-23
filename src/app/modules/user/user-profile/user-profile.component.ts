0f2353f39c8611c558d8f4e5b31a775620fcf165
Unknown <justynasadlak10@gmail.com>
2019-07-23 10:34:06 +0200

    Create user profile

---
+++ src/app/modules/user/user-profile/user-profile.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../../services/user.service';
+import {UserData} from '../../../resources/models/userData';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+
+  userData: UserData;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.userService.isAuthenticated().subscribe(
+      login => this.userService.getUserData(login)
+        .subscribe(data => this.userData = data));
+  }
+}
