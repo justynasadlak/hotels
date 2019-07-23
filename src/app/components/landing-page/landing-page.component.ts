cad227e0e1a48a930b9897d5463f87f8a76a1877
Unknown <justynasadlak10@gmail.com>
2019-07-23 10:32:30 +0200

    Improve landing page

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {BookingService} from '../../services/booking.service';
+import {Hotel} from '../../resources/models/hotel';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  searchForm: FormGroup;
+  hotels: Hotel[];
 
-  constructor() { }
+  constructor(private bookingService: BookingService, private formBuilder: FormBuilder, private router: Router) { }
 
+  ngOnInit() {
+    // this.bookingService.getAllHotels().subscribe(data => this.hotels = data);
+
+    this.searchForm = this.formBuilder.group({
+      city: [''],
+      hotel: [''],
+      checkIn: [''],
+      duration: ['']
+    });
+  }
+
+  onSubmit() {
+    console.log(this.searchForm.value);
+    this.router.navigate(['booking']);
+
+  }
 }
