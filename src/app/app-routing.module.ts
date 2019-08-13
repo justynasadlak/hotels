d53a1eb2940a8619ea2dc6873cbba0bbac77437c
Unknown <justynasadlak10@gmail.com>
2019-08-13 09:45:27 +0200

    Create route for hotel details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import {LandingPageComponent} from './containers/landing-page/landing-page.component';
 import {UserProfileComponent} from './modules/user/user-profile/user-profile.component';
 import {AuthGuardService as AuthGuard} from './auth/auth-guard.service';
+import {HotelDetailsComponent} from './containers/hotel-details/hotel-details.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent},
+  { path: 'hotel-details', component: HotelDetailsComponent },
   { path: 'my-profile', component: UserProfileComponent, canActivate: [AuthGuard] },
 ];
 
