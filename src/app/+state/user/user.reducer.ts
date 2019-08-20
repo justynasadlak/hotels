4d43468822fd02a51af1417196b5d3c36f4b8afd
Unknown <justynasadlak10@gmail.com>
2019-08-20 15:38:16 +0200

    Create user reducer

---
+++ src/app/+state/user/user.reducer.ts
@@ -0,0 +1,38 @@
+import { fromUserActions } from './user.actions';
+
+export const USER_FEATURE_KEY = 'user';
+
+export interface UserPartialState {
+  readonly [USER_FEATURE_KEY]: UserState;
+}
+
+export interface UserState {
+  username: string;
+  isLogged: boolean;
+}
+
+export const initialState: UserState = {
+  username: '',
+  isLogged: false
+};
+
+export function userReducer(
+  state: UserState = initialState,
+  action: fromUserActions.CollectiveType
+) {
+  switch (action.type) {
+    case fromUserActions.Types.GetUsername:
+      state = { ...state };
+      break;
+    case fromUserActions.Types.GetUsernameSuccess:
+      state = { ...state, username: action.payload };
+      break;
+    case fromUserActions.Types.GetIsLogged:
+      state = { ...state };
+      break;
+    case fromUserActions.Types.GetIsLoggedSuccess:
+      state = { ...state, isLogged: action.payload };
+      break;
+  }
+  return state;
+}
