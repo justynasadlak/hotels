2579172ee7f266b3465235aa6f27d1b0b6a9dc1f
Unknown <justynasadlak10@gmail.com>
2019-08-20 10:20:55 +0200

    Remove unused import

--- src/app/modules/main-page/main-page-routing.module.ts
+++ src/app/modules/main-page/main-page-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './containers/landing-page/landing-page.component';
 
 const routes: Routes = [
